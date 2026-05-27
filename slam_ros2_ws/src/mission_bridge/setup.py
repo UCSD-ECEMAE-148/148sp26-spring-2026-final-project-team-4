ae74511f9213453853e18cd85aec16d65500f7f8
EvanJayChou <evan.chou@live.com>
2026-05-26 20:59:16 -0700

    Debugged some other errors

--- slam_ros2_ws/src/mission_bridge/setup.py
+++ slam_ros2_ws/src/mission_bridge/setup.py
@@ -1,38 +1,18 @@
 from glob import glob
-import os
 
 from setuptools import find_packages, setup
 
 package_name = 'mission_bridge'
 
-
-def install_tree(source_root, destination_root):
-    installed_files = []
-    for root, _, files in os.walk(source_root):
-        relative_root = os.path.relpath(root, source_root)
-        if relative_root != '.':
-            destination_dir = os.path.join(destination_root, relative_root)
-        else:
-            destination_dir = destination_root
-        for file_name in files:
-            installed_files.append((destination_dir, [os.path.join(root, file_name)]))
-    return installed_files
-
-
-data_files = [
-    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-]
-data_files.extend(
-    install_tree('resource', os.path.join('share', package_name, 'resource'))
-)
-
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=data_files,
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/mission_bridge']),
+        ('share/mission_bridge', ['package.xml']),
+        ('share/mission_bridge/launch', glob('launch/*.launch.py')),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='evanc',
